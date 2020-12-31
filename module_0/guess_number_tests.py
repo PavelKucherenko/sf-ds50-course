81a2b10761f41b0ede4fcf4a7ee0ebd47aa93387
PavelKucherenko <47821537+PavelKucherenko@users.noreply.github.com>
2020-12-31 23:01:41 +0300

    Lower limit in test is changed to negative value and test passed

--- module_0/guess_number_tests.py
+++ module_0/guess_number_tests.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 
 class GuessNumberTest(TestCase):
     def test_game_core_v3(self):
-        for ll in range(1, 100):
+        for ll in range(-100, 100):
             for ul in range(100, 201):
                 # for max count add 2 due to specific of log function and ceil() and floor() used in game_core_v3
                 max_count = math.ceil(math.log2(ul - ll)) + 2
